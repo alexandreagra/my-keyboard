4823ab63d82e0ea4d461bf6c992762fb5d086b19
AlexandreDev <alexandreagradev@gmail.com>
2023-03-06 00:50:05 -0300

    .

--- main.js
+++ main.js
@@ -13,28 +13,28 @@ const songbookOpenClose = document.querySelector(".open-songbook");
 const songBook = document.querySelector(".songs");
 
 //ONLOAD FUNCTIONS
-window.onload = function(){
-    volumeRange()
-}
+window.onload = function () {
+  volumeRange();
+};
 
 //VOLUME RANGE CONTROLS
 
 volumeInput.addEventListener("input", () => {
-    const volumeCount = document.querySelector(".volume-count");
-    volumeCount.textContent = parseInt(volumeInput.value * 100);
+  const volumeCount = document.querySelector(".volume-count");
+  volumeCount.textContent = parseInt(volumeInput.value * 100);
 });
 
 //METRONOME RANGE CONTROLS
 
 metronomeInput.addEventListener("input", () => {
-    const metronomeCount = document.querySelector(".metronome-count");
-    metronomeCount.textContent = metronomeInput.value;
+  const metronomeCount = document.querySelector(".metronome-count");
+  metronomeCount.textContent = metronomeInput.value;
 });
 
 //HIDE AND SHOW ITEMS
 
 const hideAndShowShortcuts = () => {
-    keyShortcut.forEach((keyShortcut) =>
+  keyShortcut.forEach((keyShortcut) =>
     keyShortcut.classList.toggle("hidden-keys")
   );
 };
@@ -43,81 +43,81 @@ shortcutCheckbox.addEventListener("click", hideAndShowShortcuts);
 //
 
 const hideAndShowNotes = () => {
-    keyboardNotesText.forEach((keyNotes) => keyNotes.classList.toggle("hidden-notes"));
+  keyboardNotesText.forEach((keyNotes) =>
+    keyNotes.classList.toggle("hidden-notes")
+  );
 };
 
 notesCheckbox.addEventListener("click", hideAndShowNotes);
 //
 
 tutorialOpening.addEventListener("click", () => {
-    document.querySelector(".tutorial-modal").style.display = "flex";
+  document.querySelector(".tutorial-modal").style.display = "flex";
 });
 //
 
 tutorialClose.addEventListener("click", () => {
-    document.querySelector(".tutorial-modal").style.display = "none";
+  document.querySelector(".tutorial-modal").style.display = "none";
 });
 //
 
 songbookOpenClose.addEventListener("click", (e) => {
-    e.target.classList.toggle("changebg-songs");
-    songBook.classList.toggle("hide-songbook");
+  e.target.classList.toggle("changebg-songs");
+  songBook.classList.toggle("hide-songbook");
 });
 
 //KEYBOARD PLAYING
 
-const whiteKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
-const blackKeys = ['w', 'e', 't', 'y', 'u', 'o']
+const whiteKeys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+const blackKeys = ["w", "e", "t", "y", "u", "o"];
 
 const keyNotes = document.querySelectorAll(".key");
 const whiteKeyNotes = document.querySelectorAll(".key.white-key");
 const blackKeyNotes = document.querySelectorAll(".key.black-key");
 
-keyNotes.forEach(key => {
-    key.addEventListener('touchend', e => {
-        playTune(key)
-        return;
-    })
-    key.addEventListener('touchstart', e => e.preventDefault(), true);
+keyNotes.forEach((key) => {
+  key.addEventListener("touchend", (e) => {
+    playTune(key);
+    return;
+  });
+  key.addEventListener("touchstart", (e) => e.preventDefault(), true);
+}); //TOUCH EVENTS
 
-}) //TOUCH EVENTS
-
-keyNotes.forEach(key => {
-    key.addEventListener("click", () => playTune(key));
+keyNotes.forEach((key) => {
+  key.addEventListener("click", () => playTune(key));
 });
 
-document.addEventListener('keydown', e => {
-    if (e.repeat) return;
-    const key = e.key
-    const whiteKeyIndex = whiteKeys.indexOf(key)
-    const blackKeyIndex = blackKeys.indexOf(key)
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return;
+  const key = e.key;
+  const whiteKeyIndex = whiteKeys.indexOf(key);
+  const blackKeyIndex = blackKeys.indexOf(key);
 
-    if (whiteKeyIndex > -1) {
-        playTune(whiteKeyNotes[whiteKeyIndex])
-    } if (blackKeyIndex > -1) {
-        playTune(blackKeyNotes[blackKeyIndex])
-    }
-})
+  if (whiteKeyIndex > -1) {
+    playTune(whiteKeyNotes[whiteKeyIndex]);
+  }
+  if (blackKeyIndex > -1) {
+    playTune(blackKeyNotes[blackKeyIndex]);
+  }
+});
 
 function playTune(key) {
-    const volumeInput = document.querySelector(".volume-control input");
-    const noteAudio = document.getElementById(key.dataset.tone);
-    noteAudio.currentTime = 0
-    noteAudio.play()
-    key.classList.add('active')
-        noteAudio.addEventListener('ended', () => {
-            key.classList.remove('active')
-        })
+  const volumeInput = document.querySelector(".volume-control input");
+  const noteAudio = document.getElementById(key.dataset.tone);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add("active");
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
 }
 
-
 let volumeControl = document.querySelector(".volume-control input");
-function volumeRange (){        
-    Array.from(document.querySelectorAll("audio")).forEach(function(e){
-        e.volume = volumeControl.value;
-    })
-};
-      
-volumeControl.addEventListener('change', volumeRange);
-volumeControl.addEventListener('input', volumeRange);
+function volumeRange() {
+  Array.from(document.querySelectorAll("audio")).forEach(function (e) {
+    e.volume = volumeControl.value;
+  });
+}
 
+volumeControl.addEventListener("change", volumeRange);
+volumeControl.addEventListener("input", volumeRange);
